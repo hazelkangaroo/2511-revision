757a1067ce2f15bcd038af04e04e044b2f1d6b03
hazelnkanagroo <67770200+hazelnkanagroo@users.noreply.github.com>
2021-04-26 18:12:43 +1000

    Update heap.c

--- Heap/heap.c
+++ Heap/heap.c
@@ -1,2 +1,23 @@
 #include<stdio.h>
 
+#inclue "heap.h"
+
+Heap newHeap(int N) {
+	Heap new = malloc(sizeof(HeapRep));
+	new->items = malloc(sizeof(Item) * (N+1));
+	new->nitems = 0;
+	new->nslots = N;
+	return new;
+}
+
+
+void HeapInsert(Heap h, Item it) {
+	assert (h->nitems < h->nslots)
+	h->nitems++;
+	h->items[h->nitems] = it;
+	Fixup(h->items, h->nitems);
+}
+
+void Fixup(Item a[], int i) {
+	while (i > 1 && a[i] < a[i/2]) {
+		
